3d49e2fac17b4947a2780422d5ca69cedb9d6b49
YouOnlyLookOnce-crypto <1015306908@qq.com>
2026-05-14 20:12:41 +0800

    Update predict.py

--- models/yolo/detect/predict.py
+++ models/yolo/detect/predict.py
@@ -1,4 +1,4 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
+# Ultralytics YOLO, AGPL-3.0 license
 
 from ultralytics.engine.predictor import BasePredictor
 from ultralytics.engine.results import Results
