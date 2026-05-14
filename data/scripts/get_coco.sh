653a8fb517983e12b272912246e4e0c11dc7839f
YouOnlyLookOnce-crypto <1015306908@qq.com>
2026-05-14 21:35:24 +0800

    Update get_coco.sh

--- data/scripts/get_coco.sh
+++ data/scripts/get_coco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Ultralytics YOLO 🚀, AGPL-3.0 license
+# Ultralytics YOLO, AGPL-3.0 license
 # Download COCO 2017 dataset https://cocodataset.org
 # Example usage: bash data/scripts/get_coco.sh
 # parent
